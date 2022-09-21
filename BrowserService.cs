3307902f5f047da7f66a1dcbfdc5241dc1a50701
factubsio <65080026+factubsio@users.noreply.github.com>
2022-09-21 16:22:47 +0100

    more tooltip stuff and info things

---
+++ BrowserService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Drawing;
+using System.Threading.Tasks;
+
+public class BrowserService
+{
+    private readonly IJSRuntime _js;
+
+    public BrowserService(IJSRuntime js)
+    {
+        _js = js;
+    }
+
+    public async Task<BrowserDimension> GetDimensions()
+    {
+        return await _js.InvokeAsync<BrowserDimension>("getDimensions");
+    }
+
+    public async Task<BrowserAndElementDimension> GetBrowserAndElementDimensionsAsync(ElementReference el1, ElementReference el2)
+    {
+        return await _js.InvokeAsync<BrowserAndElementDimension>("getBrowserAndElementDimensions", el1, el2);
+    }
+}
+
+public class BrowserDimension
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
+public class BrowserAndElementDimension
+{
+    public int BrowserWidth { get; set; }
+    public int BrowserHeight { get; set; }
+
+    public float ElementTop { get; set; }
+    public float ElementBot { get; set; }
+    public float ElementLeft { get; set; }
+    public float ElementRight { get; set; }
+
+    public float ElementHeight2 { get; set; }
+}
